8cda79dd5ae83172c840a0f0a1e9706753961d46
r0697555 <38457883+r0697555@users.noreply.github.com>
2020-02-25 09:53:34 +0100

    Add files via upload

---
+++ register.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import sys
+import mysql.connector
+import socket
+
+cnx = mysql.connector.connect(user='microcontroller',password='microcontroller',host='192.168.137.4',database='syncy$
+mycur = cnx.cursor()
+
+def request_raspi(hostname):
+        get_raspi = "SELECT * FROM Raspi WHERE Raspi.naam = \'" + hostname + "\'"
+        mycur.execute(get_raspi)
+        raspirequest = mycur.fetchone()
+        if not raspirequest:
+                return 0
+        else:
+                return raspirequest
+
+def get_hostname_ip():
+        host_name = socket.gethostname()
+        host_ip = socket.gethostbyname(host_name)
+        return host_name, host_ip;
+
+
+hostname, ip = get_hostname_ip()
+raspirequest = request_raspi(hostname)
+
+if raspirequest != 0:
+        if ip not in raspirequest[2]:
+                sql1= "UPDATE Raspi SET ip=\"" + ip + "\" WHERE raspi_id=" + str(raspirequest[0])
+                mycur.execute(sql1)
+                cnx.commit()
+else:
+        sql2= "INSERT INTO Raspi (naam, ip) VALUES (%s, %s)"
+        val=(hostname, ip)
+        mycur.execute(sql2, val)
+        cnx.commit()
